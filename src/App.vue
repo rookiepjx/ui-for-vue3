d63e60c36572c2dda4d9177e48414d96e5789668
Ronan <7503697+coderPjx@user.noreply.gitee.com>
2020-11-02 11:42:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<Pjx />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Pjx from "./components/Pjx.vue";
+
+export default {
+  name: "App",
+  components: {
+    Pjx
+  },
+};
+</script>
---
+++ src/components/Pjx.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  <h1>Hello Pjx,this is Vue3</h1>
+</div>
+</template>
