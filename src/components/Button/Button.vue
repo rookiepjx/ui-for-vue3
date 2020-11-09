41bf9773622f73f959037c321940f7522b761b45
Ronan <7503697+coderPjx@user.noreply.gitee.com>
2020-11-09 18:16:49 +0800

    完成button定制化

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="content">
-  <button class="star-button" :class="classes" :disabled="disabled">
-    <span v-if="loading" class="button-loading"></span>
+  <button class="star-btn" :class="[
+        classes,
+        { 'star-btn-round': round }
+      ]" :disabled="disabled">
+    <span v-if="loading" class="star-btn-loading"></span>
     <slot />
   </button>
 </div>
 </template>
 
 <script lang="ts">
-import {
-  computed
-} from "vue";
 // 声明规定props类型和值
 declare const props: {
   theme ? : "button" | "text" | "link";
@@ -18,29 +18,10 @@ declare const props: {
   level ? : "default" | "info" | "success" | "warning" | "danger";
   disabled: boolean;
   loading: boolean;
+  round: boolean;
 };
 
 export default {
-  setup(props, context) {
-    const {
-      theme,
-      size,
-      level
-    } = props;
-    const classes = () => {
-      return {
-        [`star-theme-${theme}`]: theme,
-        [`star-size-${size}`]: size,
-        [`star-level-${level}`]: level,
-      };
-    };
-    return {
-      theme,
-      size,
-      level,
-      classes
-    };
-  },
   props: {
     theme: {
       type: String,
@@ -62,62 +43,208 @@ export default {
       type: Boolean,
       default: false,
     },
+    round: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  computed: {
+    classes() {
+      return `star-btn-${this.theme} star-btn-${this.size} star-btn-${this.level}`;
+    },
   },
 };
-// const {
-//   theme,
-//   size,
-//   level
-// } = props;
-// export const classes = computed(() => {
-//   return {
-//     [`star-theme-${theme}`]: theme,
-//     [`star-size-${size}`]: size,
-//     [`star-level-${level}`]: level,
-//   };
-// });
 </script>
 
 <style lang="scss" scoped>
+$max:999px;
+$height: 40px;
+$height-large:52px;
+$height-mini: 28px;
+$white: #fff;
+$black: #606266;
+$grey:#c8c9cc;
+$blue:#409eff;
 $border-radius: 4px;
 $border: 1px #ddd solid;
-$border-hover: 1px #c6e2ff solid;
-$default-text-hover: #40a9ff;
+$border1: 1px #c6e2ff solid;
+$default-text: #606266;
+$default-text1: #40a9ff;
 // 主题背景色
-$default: #fff;
-$default-hover: #ecf5ff;
-$primary: #409eff;
-$primary-hover: #66b1ff;
-$success: #67c23a;
-$success-hover: #85ce61;
-$info: #909399;
-$info-hover: #a6a9ad;
-$warn: #e6a23c;
-$warn-hover: #ebb563;
-$danger: #f56c6c;
-$danger-hover: #f78989;
+$default-bgc: #fff;
+$default-bgc1: #ecf5ff;
+$primary-bgc: #409eff;
+$primary-bgc1: #66b1ff;
+$success-bgc: #67c23a;
+$success-bgc1: #85ce61;
+$info-bgc: #909399;
+$info-bgc1: #a6a9ad;
+$warning-bgc: #e6a23c;
+$warning-bgc1: #ebb563;
+$danger-bgc: #f56c6c;
+$danger-bgc1: #f78989;
 
 .content {
-  >.star-button {
+
+  // 默认按钮
+  >.star-btn {
+    min-width: 100px;
+    height: $height;
     padding: 10px;
     background-color: #fff;
+    color: $black;
     border-radius: $border-radius;
     border: $border;
     cursor: pointer;
     white-space: nowrap;
 
-    &:hover,
-    &:focus {
-      border: $border-hover;
-      background-color: $default-hover;
-      color: $default-text-hover;
+    &:focus,
+    &:hover {
+      border: $border1;
+      background-color: $default-bgc;
+      color: $default-text1;
+    }
+
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
     }
   }
 
-  >.star-size-large {
+  // 文字主题按钮
+  >.star-btn-text {
+    color: #409eff;
+    background-color: $white;
+    border: none;
+
+    &:focus,
+    &:hover {
+      border: none;
+      background-color: $white;
+    }
+  }
+
+  // 超链接主题按钮
+  >.star-btn-link {
+    text-decoration: underline;
+  }
+
+  // 主要按钮
+  >.star-btn-primary {
+    color: $white;
+    background-color: $primary-bgc;
+    border: 1px solid $primary-bgc;
+
+    &:focus,
+    &:hover {
+      color: $white;
+      background-color: $primary-bgc1;
+      border: 1px solid $primary-bgc1;
+    }
+  }
+
+  // 成功按钮
+  >.star-btn-success {
+    color: $white;
+    background-color: $success-bgc;
+    border: 1px solid $success-bgc;
+
+    &:focus,
+    &:hover {
+      color: $white;
+      background-color: $success-bgc1;
+      border: 1px solid $success-bgc1;
+    }
+  }
+
+  // 信息按钮
+  >.star-btn-info {
+    color: $white;
+    background-color: $info-bgc;
+    border: 1px solid $info-bgc;
+
+    &:focus,
+    &:hover {
+      color: $white;
+      background-color: $info-bgc1;
+      border: 1px solid $info-bgc1;
+    }
+  }
+
+  // 警告按钮
+  >.star-btn-warning {
+    color: $white;
+    background-color: $warning-bgc;
+    border: 1px solid $warning-bgc;
+
+    &:focus,
+    &:hover {
+      color: $white;
+      background-color: $warning-bgc1;
+      border: 1px solid $warning-bgc1;
+    }
+  }
+
+  // 危险按钮
+  >.star-btn-danger {
+    color: $white;
+    background-color: $danger-bgc;
+    border: 1px solid $danger-bgc;
+
+    &:focus,
+    &:hover {
+      color: $white;
+      background-color: $danger-bgc1;
+      border: 1px solid $danger-bgc1;
+    }
+  }
+
+  // 大型按钮
+  >.star-btn-large {
     font-size: 24px;
-    height: 48px;
+    height: $height-large;
     padding: 0 16px;
+  }
+
+  // 小型按钮
+  >.star-btn-mini {
+    font-size: 12px;
+    height: $height-mini;
+    padding: 0 8px;
+  }
+
+  // 圆角按钮
+  >.star-btn-round {
+    // 为了适合各个尺寸按钮高度50%的圆角，设置为最大值
+    border-radius: $max;
+  }
+
+  // 加载按钮
+  .star-btn-loading {
+    display: inline-block;
+    width: 14px;
+    height: 14px;
+    margin-right: 6px;
+    vertical-align: top;
+    border-radius: 50%;
+    border-width: 2px;
+    border-style: solid;
+    border-color: $blue $blue $blue transparent;
+    animation: star-btn-loading 1s infinite linear;
+  }
+
+  @keyframes star-btn-loading {
+    0% {
+      transform: rotate(0deg)
+    }
+
+    100% {
+      transform: rotate(360deg)
+    }
   }
 }
 </style>
--- src/components/Button/ButtonDemo.vue
+++ src/components/Button/ButtonDemo.vue
@@ -2,35 +2,60 @@
 <div class="content">
   <h1 class="title">Button组件</h1>
   <div class="box">
-    <h3>常规用法</h3>
+    <h3>基础用法</h3>
     <div class="cpn">
       <Button>默认按钮</Button>
+      <Button level="primary">主要按钮</Button>
+      <Button theme="text">文本按钮</Button>
+      <Button round>圆角按钮</Button>
+      <Button size="mini">小型按钮</Button>
+      <Button disabled>禁用按钮</Button>
+      <Button loading>加载中</Button>
     </div>
   </div>
   <div class="box">
-    <h3>不同主题</h3>
+    <h3>主题按钮</h3>
     <div class="cpn">
-      <Button>普通按钮</Button>
+      <Button>默认按钮</Button>
       <Button theme="text">文本按钮</Button>
       <Button theme="link">链接按钮</Button>
     </div>
   </div>
   <div class="box">
-    <h3>不同尺寸</h3>
+    <h3>尺寸按钮</h3>
     <div class="cpn">
-      <Button>普通按钮</Button>
+      <Button>默认按钮</Button>
       <Button size="large">大型按钮</Button>
       <Button size="mini">小型按钮</Button>
     </div>
   </div>
   <div class="box">
-    <h3>不同级别</h3>
+    <h3>级别按钮</h3>
     <div class="cpn">
       <Button>默认按钮</Button>
+      <Button level="primary">主要按钮</Button>
       <Button level="info">信息按钮</Button>
       <Button level="success">成功按钮</Button>
       <Button level="warning">警示按钮</Button>
       <Button level="danger">危险按钮</Button>
+    </div>
+  </div>
+  <div class="box">
+    <h3>圆角按钮</h3>
+    <div class="cpn">
+      <Button>默认按钮</Button>
+      <Button level="primary" round>主要按钮</Button>
+      <Button level="info" round>信息按钮</Button>
+      <Button level="success" round>成功按钮</Button>
+      <Button level="warning" round>警示按钮</Button>
+      <Button level="danger" round>危险按钮</Button>
+    </div>
+  </div>
+  <div class="box">
+    <h3>加载按钮</h3>
+    <div class="cpn">
+      <Button>默认按钮</Button>
+      <Button loading>加载中</Button>
     </div>
   </div>
 </div>
@@ -58,6 +83,7 @@ export default {
     >.cpn {
       display: flex;
       flex-wrap: wrap;
+      align-items: center;
       background-color: #fff;
     }
   }
